77f2f5071aea212f7f88a565cfaddc98fef2f97d
Mahesh <moholkarmahesh@aca.edu.in>
2023-07-07 21:19:39 +0530

    Made Navbar and Post-Card Component Dynamic

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { PostsService } from 'src/app/services/posts.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  featuredArray: Array<any> = [];
+  latestPostsArray: Array<any> = [];
+  constructor(private postService: PostsService) {}
 
+  @Input() postData: any;
+
+  ngOnInit(): void {
+    this.postService.loadFeatured().subscribe((data) => {
+      this.featuredArray = data;
+    });
+    this.postService.loadLatest().subscribe((data) => {
+      this.latestPostsArray = data;
+    });
+  }
 }
---
+++ src/app/services/posts.service.ts
@@ -0,0 +1,59 @@
+import { Injectable } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
+import { map } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class PostsService {
+  constructor(private firestore: AngularFirestore) {}
+
+  loadFeatured() {
+    return this.firestore
+      .collection('posts', (ref) =>
+        ref.where('isFeatured', '==', true).limit(4)
+      )
+      .snapshotChanges()
+      .pipe(
+        map((actions) => {
+          return actions.map((a) => {
+            const data = a.payload.doc.data();
+            const id = a.payload.doc['id'];
+            return { id, data };
+          });
+        })
+      );
+  }
+
+  loadLatest() {
+    return this.firestore
+      .collection('posts', (ref) => ref.orderBy('createdAt'))
+      .snapshotChanges()
+      .pipe(
+        map((actions) => {
+          return actions.map((a) => {
+            const data = a.payload.doc.data();
+            const id = a.payload.doc['id'];
+            return { id, data };
+          });
+        })
+      );
+  }
+
+  loadCategoryPost(categoryId: any) {
+    return this.firestore
+      .collection('posts', (ref) =>
+        ref.where('category.categoryId', '==', categoryId).limit(4)
+      )
+      .snapshotChanges()
+      .pipe(
+        map((actions) => {
+          return actions.map((a) => {
+            const data = a.payload.doc.data();
+            const id = a.payload.doc['id'];
+            return { id, data };
+          });
+        })
+      );
+  }
+}
