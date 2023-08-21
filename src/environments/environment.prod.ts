d307c6327cdc237f15f3cce298b2a317f3ccefab
Mahesh <moholkarmahesh@aca.edu.in>
2023-08-21 21:04:10 +0530

    Added Capacitor - Android App

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,11 @@
 export const environment = {
   production: 'true',
   firebaseConfig: {
-    apiKey: 'AIzaSyAoBmC7LLdxyea_30_-BDEXD9RMTH-X3FE',
-    authDomain: 'ang-blog-app-f6621.firebaseapp.com',
-    projectId: 'ang-blog-app-f6621',
-    storageBucket: 'ang-blog-app-f6621.appspot.com',
-    messagingSenderId: '251512835754',
-    appId: '1:251512835754:web:fc94a38f2cee86ab0b8ad3',
+    apiKey: 'AIzaSyD3Bic3QZvptKyEC94B9kUWcssV8ij84qo',
+    authDomain: 'campus-insider-edu.firebaseapp.com',
+    projectId: 'campus-insider-edu',
+    storageBucket: 'campus-insider-edu.appspot.com',
+    messagingSenderId: '586766485291',
+    appId: '1:586766485291:web:30f09b79bb93aa4afb276e',
   },
 };
